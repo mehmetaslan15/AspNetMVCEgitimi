a18a17b49d35b94f4e3396d47ef791249f63af83
mehmet.aslan <mehmet.aslan@TEDAS-PC2788>
2022-06-18 13:00:59 +0300

    Add project files.

---
+++ AspNetMVCEgitimi.NETFramework/Controllers/Mvc07PartialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCEgitimi.NETFramework.Controllers
+{
+    public class Mvc07PartialController : Controller
+    {
+        // GET: Mvc07Partial  : Partial ile 1 sayfa içerisindeki bölümleri parçalara ayırıp kullanabiliriz.
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
