1501b6dfe23270751ebda6bb7352ad9db5b5f761
Mehmet Aslan <aslan.memett@gmail.com>
2022-06-19 13:05:13 +0300

    19-06-2022 mvc cookielerdeyiz, ders sonu

---
+++ AspNetMVCEgitimi.NETFramework/Controllers/Mvc11ModelValidationController.cs
@@ -0,0 +1,112 @@
+﻿using AspNetMVCEgitimi.NETFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCEgitimi.NETFramework.Controllers
+{
+    public class Mvc11ModelValidationController : Controller
+    {
+        // GET: Mvc11ModelValidation  : Validation ile model class larımızda doğrulama işlemi yaparız. 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult YeniUye()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUye(Uye uye)
+        {
+            if (ModelState.IsValid)    //ModelState IsValid parametreden gelen uye nesnesinin validation kurallarını denetler
+                                       //ve kurallara uyulmuşsa kod bloğundaki kodları çalıştırır
+            {
+                ViewBag.UyeBilgi = $"Üye Adı : {uye.Ad} <hr /> Üye Soyad : {uye.Soyad} <hr /> " +
+                $"Üye Mail : {uye.Email} <hr /> Üye TC : {uye.TcKimlikNo}";
+            }
+
+            return View();
+        }
+        public ActionResult UyeDuzenle(int? id)  //üye düzenlemenin get metodunda id parametresi beklenir çünkü bu id ye göre
+                                                 // ilgili üye bilgileri ekrana getirilir. (int? Soru işareti boş geçilebilir
+                                                 //anlamına geliyor. Bu da önemli.
+        {
+            Uye uye = new Uye()
+            {
+                Id = 1,
+                Ad = "Fatih",
+                Soyad = "Sultan",
+                Email = "fatih@sultan.net",
+                TcKimlikNo = "52651307380"
+            };
+            return View(uye);
+        }
+        [HttpPost]
+        public ActionResult UyeDUzenle(Uye uye)
+        {
+            if (ModelState.IsValid)    //ModelState IsValid parametreden gelen uye nesnesinin validation kurallarını denetler
+                                       //ve kurallara uyulmuşsa kod bloğundaki kodları çalıştırır
+            {
+                ViewBag.UyeBilgi = $"Üye Adı : {uye.Ad} <hr /> Üye Soyad : {uye.Soyad} <hr /> " +
+                $"Üye Mail : {uye.Email} <hr /> Üye TC : {uye.TcKimlikNo}";
+            }
+
+            return View(uye);  //uye nesnesini veiw a geri yolla. 
+        }
+        public ActionResult UyeListesi()
+        {
+            var uyeListesi = new List<Uye> {
+            new Uye()
+            {
+                Id = 1,
+                Ad = "Fatih",
+                Soyad = "Sultan",
+                Email = "fatih@sultan.net",
+                TcKimlikNo = "52651307380"
+            },
+            new Uye()
+            {
+                Id = 2,
+                Ad = "Mehmet",
+                Soyad = "Aslan",
+                Email = "fatihsdas@sultan.net",
+                TcKimlikNo = "12351307380"
+            },
+            new Uye()
+            {
+                Id = 3,
+                Ad = "Ömer",
+                Soyad = "Aslan",
+                Email = "omer@sultan.net",
+                TcKimlikNo = "31251307380"
+            }
+            };
+            return View(uyeListesi);  //view a uyelistesi nesnesini gönderdik. 
+        }
+        public ActionResult UyeSil(int? id)  //üye düzenlemenin get metodunda id parametresi beklenir çünkü bu id ye göre
+                                                 // ilgili üye bilgileri ekrana getirilir. (int? Soru işareti boş geçilebilir
+                                                 //anlamına geliyor. Bu da önemli.
+        {
+            Uye uye = new Uye()
+            {
+                Id = 1,
+                Ad = "Fatih",
+                Soyad = "Sultan",
+                Email = "fatih@sultan.net",
+                TcKimlikNo = "52651307380"
+            };
+            return View(uye);
+        }
+        [HttpPost]
+        public ActionResult Uyesil()
+        {
+            //burada kayıt veritabanından silinir ve sayfa liste ekranına yönlendirilir. 
+            TempData["Mesaj"] = "<div class = 'alert alert-danger'>Kayıt Silindi!</div>";
+            return View();
+        }
+
+    }
+}
---
+++ AspNetMVCEgitimi.NETFramework/Models/Uye.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //Model validation için eklememiz gerekiyor
+
+namespace AspNetMVCEgitimi.NETFramework.Models
+{
+    public class Uye
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Lütfen Adınızı Giriniz!"), StringLength(50)]   //Required attribute ü property nin üzerine eklenir ve o property i zorunlu hale getirir. 
+        public string Ad { get; set; }
+        [Required, StringLength(50)] 
+        public string Soyad { get; set; }
+        [Required]
+        [EmailAddress]  //aşağıdaki alanın email tipinde olmasını sağlar. 
+        public string Email { get; set; }
+        [Phone]
+        public string Telefon { get; set; }
+        [Required]
+        [Display(Name = "TC Kimlik Numarası")]  //Display ile property adını ekranda farklı isimle gösterebiliriz
+        [MinLength(11, ErrorMessage = "TC Kimlik Numarası en az 11 karakter olmalıdır")]  //Bu alana girilebilecek minimum karakter sayısı 
+        [MaxLength(11, ErrorMessage = "TC Kimlik Numarası en fazla 11 karakter olmalıdır")]  //Bu da 11 karakterden fazla olmaması için yazdık. 
+        public string TcKimlikNo { get; set; }
+        [Display(Name = "Doğum Tarihi"), DisplayFormat(DataFormatString = "{0:dd.MM.yyy}"), DataType(DataType.Date)]
+        public DateTime DogumTarihi { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
+        public string KullaniciAdi { get; set; }
+        [Display(Name = "Şifre"), DataType(DataType.Password)]
+        [StringLength(10, ErrorMessage = "{0} {2} karakterden uzun olmalı", MinimumLength = 5)]
+        public string Sifre { get; set; }
+        [Display(Name = "Şifrenizi Tekrar Giriniz"), DataType(DataType.Password)]
+        [Compare("Sifre")]  //SifreTekrar alanını Sifre alanı ile karşılaştır
+        public string SifreTekrar { get; set; }
+
+    }
+}
