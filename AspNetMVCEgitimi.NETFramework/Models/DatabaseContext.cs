2b050f3da63f79963863f477937aa01c1190df64
Mehmet Aslan <aslan.memett@gmail.com>
2022-06-26 13:00:08 +0300

    26.05.2022 - Category Create İşlemleri

---
+++ AspNetMVCEgitimi.NETFramework/Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;  //EF kütüphanesini dahil ediyoruz. 
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVCEgitimi.NETFramework.Models
+{
+    public class DatabaseContext : DbContext // DatabaseContext bizim db setlerimizi tutacak classımız. DbContext ise Entity frameworkün vertabanı işlemleri için bize sunduğu class
+    {
+        public DbSet<Product>  Products { get; set; }  //Veritabanı tablolarımızı temsil edecek classlarımızı dbset olarak tanımlıyoruz. 
+        public DbSet<Category> Categories { get; set; }
+    }
+}
