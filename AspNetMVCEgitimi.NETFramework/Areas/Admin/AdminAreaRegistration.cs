1fcb5a6937d56243606abf1bfadba0fc7ca85d1a
Mehmet Aslan <aslan.memett@gmail.com>
2022-07-16 12:57:45 +0300

    16-7-2022, Product Edit

--- AspNetMVCEgitimi.NETFramework/Areas/Admin/AdminAreaRegistration.cs
+++ AspNetMVCEgitimi.NETFramework/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace AspNetMVCEgitimi.NETFramework.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new {Controller = "Categories", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- AspNetMVCEgitimi.NETFramework/Models/Product.cs
+++ AspNetMVCEgitimi.NETFramework/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace AspNetMVCEgitimi.NETFramework.Models
     public class Product
     {
         public int Id { get; set; }
+        [Display(Name= "Ürün Adı")]
         public string Name { get; set; }
+        [Display(Name = "Stok")]
         public int Stock { get; set; }
+        [Display(Name = "Ürün Fiyatı")]
         public decimal Price { get; set; }
+        [Display(Name = "Ürün Resmi")]
         public string Image { get; set; }
     }
 }
